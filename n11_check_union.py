449f7d43a7ff8bfa1ea17aa24ce4bfb06e1f1647
Vladimir Groza <vladimir.groza@gmail.com>
2019-08-28 16:59:57 +0200

    check union + dice_agreement; add union_blend

--- n11_check_union.py
+++ n11_check_union.py
@@ -3,13 +3,16 @@ __author__ = "bobbqe"
 import os
 import os.path as osp
 import gc
+import operator
+from multiprocessing import Pool
 
 import numpy as np
-from n03_loss_metric import dice_coef_metric_batch
+from n03_loss_metric import dice_coef_metric_batch, dice_coef_metric
 from n09_submit import binarize_sample
 
+# NCORE = 18
 
-def check_mean_fold(fold):
+def check_mean_fold(fold, dilation, min_size_thresh):
 
     if "+" in model_name:
         model_lst = model_name.split("+")
@@ -46,19 +49,25 @@ def check_mean_fold(fold):
 
     fold_mean_bin = []
     for i in range(fold_mean.shape[0]):
-        fold_mean_bin.append(binarize_sample((fold_mean[i], mask_thresh, min_size_thresh, dilation)))
+        agree_dice = dice_coef_metric(fold_mean[i], gts[i])
+        # print (f"Pic dice on mean: {agree_dice}  and agreement: {agreement_tta}")
+        if agree_dice > agreement_tta:
+            dilation_c = 0
+        else:
+            dilation_c = dilation
+
+        fold_mean_bin.append(binarize_sample((fold_mean[i], mask_thresh, min_size_thresh, dilation_c)))
     fold_mean_bin = np.array(fold_mean_bin)
 
     return dice_coef_metric_batch(fold_mean_bin, gts)
 
 
-def check_binary_union_fold(fold):
+def check_binary_union_fold(fold, dilation, min_size_thresh):
 
     if "+" in model_name:
         model_lst = model_name.split("+")
     else:
         model_lst = [model_name]
-
 
     models_mean = []
     models_mean_bin = []
@@ -84,9 +93,15 @@ def check_binary_union_fold(fold):
     for j in range(models_mean.shape[0]):
         model_mean_bin = []
         for k in range(models_mean[j].shape[0]):
-            model_mean_bin.append(
-                binarize_sample((models_mean[j][k], mask_thresh, min_size_thresh, dilation))
-            )
+            agree_dice = dice_coef_metric(models_mean[j][k], gts[k])
+            # print (f"Pic dice on union: {agree_dice}  and agreement: {agreement_tta}")
+            if agree_dice > agreement_tta:
+                dilation_c = 0
+            else:
+                dilation_c = dilation
+            sample_mean_bin = binarize_sample((models_mean[j][k], mask_thresh, min_size_thresh, dilation_c))
+
+            model_mean_bin.append(sample_mean_bin)
         models_mean_bin.append(model_mean_bin)
 
 
@@ -98,8 +113,8 @@ def check_binary_union_fold(fold):
     return dice_coef_metric_batch(models_union_bin, gts)
 
 
-def main(dilation_in, min_size_thresh_in):
-    global model_name, n_fold, mask_thresh, min_size_thresh, dilation, dumps_dir
+def main(dilation_in, min_size_thresh_in, agreement_in):
+    global model_name, n_fold, mask_thresh, dumps_dir, agreement_tta
     # model_name = "sx50+sx101+se154"
     model_name = "sx50+sx101"
     n_fold = 8
@@ -107,27 +122,44 @@ def main(dilation_in, min_size_thresh_in):
     mask_thresh = 0.5
     min_size_thresh = min_size_thresh_in
     dilation = dilation_in
+    agreement_tta = agreement_in
     mean_scores, bin_union_scores = [], []
 
-    # print(f"Validation DICEs over 2 models :")
-    # print(f"                    MEAN        :      UNION  ")
     for fold in range(n_fold):
-        mean_score = check_mean_fold(fold)
+        mean_score = check_mean_fold(fold, dilation, min_size_thresh)
         mean_scores.append(mean_score)
-        bin_union_score = check_binary_union_fold(fold)
+        bin_union_score = check_binary_union_fold(fold, dilation, min_size_thresh)
         bin_union_scores.append(bin_union_score)
         # print(f"Fold {fold} :    {mean_score}   :  {bin_union_score}")
         
-    print(f"Mean validation DICE over 2 models: dilation: {dilation}  min_size_thresh: {min_size_thresh}")
+    print(f"Mean validation DICE over 2 models: dilation: {dilation}  min_size_thresh: {min_size_thresh} agreement_tta: {agreement_tta}")
     print(f" {np.mean(mean_scores)}   :  {np.mean(bin_union_scores)}")
+
+    return np.mean(mean_scores), np.mean(bin_union_scores)
 
 
 if __name__ == '__main__':
-    dilations = [0,1,2]
-    thresh_mins = [1000, 1500, 2000]
-    for dil in dilations:
-        for thresh_min in thresh_mins:
-            main(dil, thresh_min)
+    global stats_mean_dic, stats_union_dic
+    stats_mean_dic = {}
+    stats_union_dic = {}
+    dilations = [1,2]
+    # thresh_mins = np.linspace(1000, 1600, num=7)
+    min_size_thresh = 1500
+    agreement_th = np.linspace(0.7, 0.95, num=6)
+    for dilation in dilations:
+    #     for min_size_thresh in thresh_mins:
+        for agreement in agreement_th:
+        # with Pool() as p:
+        #     scores = p.map(main, agreement_th)
+            scores = main(dilation, min_size_thresh, agreement)
+
+        stats_mean_dic[f'Dil: {dilation}  Min_size: {min_size_thresh} Dice_agree: {agreement}'] = scores[0]
+        stats_union_dic[f'Dil: {dilation}  Min_size: {min_size_thresh} Dice_agree: {agreement}'] = scores[1]
+
+    max_stats_mean = max(stats_mean_dic.items(), key=operator.itemgetter(1))
+    max_stats_union = max(stats_union_dic.items(), key=operator.itemgetter(1))
+    print(f"Best mean validation DICE over 2 models: {max_stats_mean[1]}  with set up: {max_stats_mean[0]}")
+    print(f"Best union validation DICE over 2 models: {max_stats_union[1]}  with set up: {max_stats_union[0]}")
 
 
 
---
+++ n12_union_blend.py
@@ -0,0 +1,79 @@
+__author__ = "bobbqe"
+
+import numpy as np
+import pandas as pd
+import os
+import glob
+import tqdm
+from scipy.misc import imread, imresize, imsave
+from scipy import ndimage
+from n02_utils import mask_to_rle, rle2mask
+
+def main():
+	os.makedirs('outs/', exist_ok=True)
+	model_name = 'blend_union_sx50_sx101_se154'
+
+	sample_df = pd.read_csv("tables/sample_submission.csv")
+	sample_df = sample_df.drop_duplicates('ImageId', keep='last').reset_index(drop=True)
+
+	masks_ = sample_df.groupby('ImageId')['ImageId'].count().reset_index(name='N')
+	###
+
+	total_preds = np.zeros([len(sample_df),1024,1024])
+
+	subs=os.listdir('submissions/')
+
+	pred_dict = {}
+
+	for jj in range(len(subs)):
+		print (subs[jj])
+		df = pd.read_csv(f'submissions/{subs[jj]}')
+		
+		for index, row in tqdm.tqdm(df.iterrows(), total=len(df)):
+			image_id = row['ImageId']
+			annot = row["EncodedPixels"].strip()
+
+			if (annot != '-1'):
+				mask = rle2mask(annot, 1024, 1024)
+			else:
+				mask = np.zeros((1024,1024))
+			
+			if image_id in pred_dict:
+				pred_dict[image_id] += mask
+			else:
+				pred_dict[image_id] = mask
+		del df
+	
+	print (len(pred_dict))
+
+#################################################################
+
+	threshold_list = [0,1]  # 0 - if union, 1 - if with certainty of 2 for many models
+
+	for threshold in threshold_list:
+		sublist = []
+		for index, row in tqdm.tqdm(sample_df.iterrows(), total=len(sample_df)):
+			image_id = row['ImageId']
+			pred = pred_dict[image_id]
+
+			if pred.sum() > 0:
+
+				out_cut = np.copy(pred)
+				out_cut[np.nonzero(out_cut<=threshold)]=0.
+				out_cut[np.nonzero(out_cut>threshold)]=1.
+
+				out_cut = ndimage.binary_fill_holes(out_cut).astype(out_cut.dtype)
+
+				# imsave(model_name+'/'+image_id + '.png', out_cut)
+
+				rle = mask_to_rle(out_cut, 1024, 1024)
+				sublist.append([image_id, rle])
+			else:
+				rle = " -1"
+				sublist.append([image_id, rle])
+
+			submission_df = pd.DataFrame(sublist, columns=sample_df.columns.values)
+			submission_df.to_csv(f'outs/union_submission_{model_name}_{threshold}.csv', index=False)
+
+if __name__ == "__main__":
+	main()
