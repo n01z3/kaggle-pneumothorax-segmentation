05584933bef75aad7b91ea8e2cf9257f17ad3b4e
Vladimir Groza <vladimir.groza@gmail.com>
2019-08-29 16:17:10 +0200

    handy model name in script

--- b01_multi_folds_dl6.sh
+++ b01_multi_folds_dl6.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-MODEL=%1
+# MODEL=%1
 
 train_fold() {
   echo ${1} ${2}
-  CUDA_VISIBLE_DEVICES=${2} python n15_train.py --fold=${1} --net $MODEL | tee ${1}'fold_1.log'
+  CUDA_VISIBLE_DEVICES=${2} python n15_train.py --fold=${1} --net sx50_st2 | tee ${1}'fold_1.log'
 }
 export -f train_fold
 parallel -j4 --line-buffer train_fold {} '$(({%} % 4))' ::: {0..7}
